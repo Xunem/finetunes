6208121d3ca9a4cea9b7ef9762c2f7afd4429ea8
xunem <marianschaub@gmail.com>
2018-03-28 15:20:37 +0200

    sptoken

--- src/app/tunes/tunes.component.ts
+++ src/app/tunes/tunes.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { db, model } from 'baqend';
+import {Http, Response, RequestOptions, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
 
 @Component({
   selector: 'tunes',
@@ -8,10 +11,15 @@ import { db, model } from 'baqend';
   styleUrls: ['./tunes.component.scss']
 })
 export class TunesComponent implements OnInit {
+  client_id = "3a95841906b141b9a5e10014744ae040";
+  client_secret = "36ec021cbd9948749d0a02abf9deaeba";
+  private accessToken: any;
+  private tokenType: string;
 
+  private spApiUrl = "https://api.spotify.com/v1/search?q=herbert&type=track"
   public tunes: Array<model.Tune>;
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private http: Http) {
     db.ready().then(() => {
         if (!db.User.me) {
           this.router.navigate(['/signup']);
@@ -20,14 +28,57 @@ export class TunesComponent implements OnInit {
             .find()
             .resultList()
             .then(tunes => this.tunes = tunes);
+
+            this.login().subscribe(() => {
+              this.searchAlbums();
+            });
+           
         }
         
     });
 
   }
 
+  login() {
+    let authorizationTokenUrl = `https://accounts.spotify.com/api/token`;
+
+    let header = new Headers();
+    header.append('Authorization', 'Basic  ' + btoa(this.client_id + ':' + this.client_secret));
+    header.append('Content-Type', 'application/x-www-form-urlencoded;');
+
+    let options = new RequestOptions({ headers: header });
+    let body = 'grant_type=client_credentials';
+    return this.http.post(authorizationTokenUrl, body, options)
+      .map(data => data.json())
+      .do(token => {
+        this.accessToken = token.access_token;
+        this.tokenType = token.token_type;
+      }, error => console.log(error));
+    
+  }
+
+  searchAlbums() {
+    const options = this.getOptions();
+    return this.http.get(this.spApiUrl, options)
+      .map(res => res.json())
+      .subscribe(data => {
+        console.log(data);
+      });
+  }
+
   ngOnInit() {
     
+  }
+
+  private getOptions() {
+    console.log(this.accessToken);
+    console.log(this.tokenType);
+
+    let header = new Headers();
+    header.append('Authorization', this.tokenType + ' ' + this.accessToken);
+    let options = new RequestOptions({ headers: header });
+
+    return options;
   }
 
 }
--- src/app/tunes/tunes.module.ts
+++ src/app/tunes/tunes.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpModule } from '@angular/http';
 
 import { TunesComponent } from './tunes.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    HttpModule 
   ],
   declarations: [
+    TunesComponent
+  ],
+  providers: [
     TunesComponent
   ]
 })
